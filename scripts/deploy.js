2c0498db17ee4f50f8ece0498abaca7de7987269
mrelvn07 <mrelvn07@gmail.com>
2024-08-08 14:07:11 +0000

    feat: initiated the project

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,31 @@
-const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
 
-  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik!!"]);
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  await contract.waitForDeployment();
+  const Swisstronik = await ethers.getContractFactory('Swisstronik');
+  const swisstronik = await Swisstronik.deploy();
+  await swisstronik.waitForDeployment(); 
+  console.log('Non-proxy Swisstronik deployed to:', swisstronik.target);
+  fs.writeFileSync("contract.txt", swisstronik.target);
 
-  console.log(`This is your deployed contract address ${contract.target}`);
+  console.log(`Deployment transaction hash: https://explorer-evm.testnet.swisstronik.com/address/${swisstronik.target}`);
+
+  console.log('');
+  
+  const upgradedSwisstronik = await upgrades.deployProxy(Swisstronik, ['Hello Swisstronik from Happy Cuan Airdrop!!'], { kind: 'transparent' });
+  await upgradedSwisstronik.waitForDeployment(); 
+  console.log('Proxy Swisstronik deployed to:', upgradedSwisstronik.target);
+  fs.writeFileSync("proxiedContract.txt", upgradedSwisstronik.target);
+
+  console.log(`Deployment transaction hash: https://explorer-evm.testnet.swisstronik.com/address/${upgradedSwisstronik.target}`);
 }
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
